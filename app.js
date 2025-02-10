95f36d86a9d5366318a43270f6958eb81672c98d
MlodziutkiS <sados4f@gmail.com>
2025-02-10 22:26:11 +0100

    redirect to hide bare html content

--- app.js
+++ app.js
@@ -5,19 +5,31 @@ const path = require('path')
 
 const app = express()
 const port = 3000
+
 let data={body:"Sorry wttr.in did not respond <br> but the temperature outside should be more than -273C and less than 1400C", age:0};
 
-app.use(cors())
+//app.use(cors("https://localhost:3000/"))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "./index.html"))
   })
 
 app.get('/home', (req, res) => {
+  if(req.get('sec-fetch-site')=='none'){
+    console.log("redirected")
+    res.redirect(301, 'http://localhost:3000')
+    return;
+  }
   res.sendFile(path.join(__dirname, "./home.html"))
 })
 
 app.get('/weather', (req, res) => {
+
+  if(req.get('sec-fetch-site')=='none'){
+    console.log("redirected")
+    res.redirect(301, 'http://localhost:3000')
+    return;
+  }
 
     if(Date.now()-data.age > 6000){
         data.age=Date.now();
@@ -37,6 +49,13 @@ app.get('/weather', (req, res) => {
   })
 
   app.get('/about', (req, res) => {
+
+    if(req.get('sec-fetch-site')=='none'){
+      console.log("redirected")
+      res.redirect(301, 'http://localhost:3000')
+      return;
+    }
+
     res.send('Htmx is the way for <i>fast</i> websites')
   })
 
